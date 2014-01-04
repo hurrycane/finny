8cfc82a832d01d6157e6194a8548ced5c16ad8da
Bogdan Gaza <bc.gaza@gmail.com>
2014-01-04 21:32:10 +0200

    Added empty templates and logic to create them when generating new structure

--- finny/commands/generate_structure.py
+++ finny/commands/generate_structure.py
@@ -1,5 +1,7 @@
 import os
 
+from jinja2 import Environment, PackageLoader
+
 from finny.command import Command
 
 BASE_FOLDER_TEMPLATES = [
@@ -13,12 +15,10 @@ CONFIG_INITIALIZERS_TEMPLATES = [ "app.py" ]
 CONFIG_RUNNERS_TEMPLATES = [ "default.py" ]
 CONFIG_TEMPLATES = [
   "boot.py",
-  "development.py.sample"
+  "development.py.sample",
   "test.py.sample",
   "production.py.sample"
 ]
-
-TEMPLATES_PATH = ""
 
 class GenerateStructure(Command):
 
@@ -29,22 +29,33 @@ class GenerateStructure(Command):
   def _template(self, template_name, path):
     pass
 
-  def _copy_template(self, source, src, dst):
-    pass
+  def _copy_templates(self, source, src, dst):
+    env = Environment(loader=PackageLoader('finny.commands', 'templates/' + src))
+    for item in source:
+      template = env.get_template("%s.jinja" % item)
+      output = template.render(name=self.name)
+
+      path = dst + "/" + item
+      dirname = os.path.dirname(path)
+
+      if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+      with open(path, "w+") as f:
+        f.write(output)
 
   def run(self):
-    os.mkdir(self.path, 0755)
+    os.makedirs(self.path, 0755)
 
-    self._copy_templates(BASE_FOLDER_TEMPLATES, TEMPLATES_PATH, self.path)
+    self.env = Environment(loader=PackageLoader('finny.commands', 'templates'))
 
-    self._copy_templates(CONFIG_INITIALIZERS_TEMPLATES,
-                         TEMPLATES_PATH + "initializers",
+    self._copy_templates(BASE_FOLDER_TEMPLATES, "", self.path)
+
+    self._copy_templates(CONFIG_INITIALIZERS_TEMPLATES, "initializers",
                          "%s/%s/initializers" % (self.path, self.name) )
 
-    self._copy_templates(CONFIG_RUNNERS_TEMPLATES,
-                         TEMPLATES_PATH + "runners",
+    self._copy_templates(CONFIG_RUNNERS_TEMPLATES, "runners",
                          "%s/%s/runners" % (self.path, self.name) )
 
-    self._copy_templates(CONFIG_TEMPLATES,
-                         TEMPLATES_PATH + "config",
+    self._copy_templates(CONFIG_TEMPLATES, "config",
                          "%s/%s/" % (self.path, self.name) )

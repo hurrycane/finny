085bc7787eac2d44fd4c19c8161709b20dc324be
Bogdan Gaza <bc.gaza@gmail.com>
2014-01-04 20:39:21 +0200

    Copy template structure for the create command

--- finny/commands/generate_structure.py
+++ finny/commands/generate_structure.py
@@ -2,44 +2,49 @@ import os
 
 from finny.command import Command
 
+BASE_FOLDER_TEMPLATES = [
+  ".gitignore",
+  "requirements.txt",
+  "README.md",
+  "manage.py"
+]
+
+CONFIG_INITIALIZERS_TEMPLATES = [ "app.py" ]
+CONFIG_RUNNERS_TEMPLATES = [ "default.py" ]
+CONFIG_TEMPLATES = [
+  "boot.py",
+  "development.py.sample"
+  "test.py.sample",
+  "production.py.sample"
+]
+
+TEMPLATES_PATH = ""
+
 class GenerateStructure(Command):
 
   def __init__(self, name, path):
     self.name = name
     self.path = path
 
+  def _template(self, template_name, path):
+    pass
+
+  def _copy_template(self, source, src, dst):
+    pass
+
   def run(self):
     os.mkdir(self.path, 0755)
 
-    """
-    You need to create:
+    self._copy_templates(BASE_FOLDER_TEMPLATES, TEMPLATES_PATH, self.path)
 
-    .gitignore
-    requirements.txt
-    README.md
+    self._copy_templates(CONFIG_INITIALIZERS_TEMPLATES,
+                         TEMPLATES_PATH + "initializers",
+                         "%s/%s/initializers" % (self.path, self.name) )
 
-    manage.py
+    self._copy_templates(CONFIG_RUNNERS_TEMPLATES,
+                         TEMPLATES_PATH + "runners",
+                         "%s/%s/runners" % (self.path, self.name) )
 
-    {{ app_name }}:
-      initializers:
-        app.py
-
-      boot.py
-
-      runners:
-        default.py
-
-      development.py
-      test.py
-      production.py
-
-    monitors:
-      api.py
-      models.py
-      tests:
-        fixtures/
-        test_base.py
-
-    utils.py
-
-    """
+    self._copy_templates(CONFIG_TEMPLATES,
+                         TEMPLATES_PATH + "config",
+                         "%s/%s/" % (self.path, self.name) )

3d642671e66a79d95ac7e16967f3bc77a0adb965
Bogdan Gaza <bc.gaza@gmail.com>
2014-01-05 19:21:05 +0200

    Added proper load runners and a method that for 404 method shows all the routes

--- finny/boot/boot_helper.py
+++ finny/boot/boot_helper.py
@@ -1,10 +1,53 @@
 import os
+from importlib import import_module
+
+import inflect
+
+from flask import url_for
 
 STAGES = [ "development", "test", "production" ]
 
-def load_runner(app, runner):
+def load_runner(name, app, runner):
+  pluralize = inflect.engine()
   # loads the runner that has a list of endpoints
-  pass
+  module = import_module("%s.runners.%s" % (name, runner))
+
+  for endpoint in module.ENDPOINTS:
+    endpoint_plural = pluralize.plural(endpoint)
+
+    module = import_module("resources.%s.api" % endpoint)
+    klass = getattr(module, "%sView" % endpoint_plural.capitalize())
+    # TODO: Think at something better that this hardcoded prefix
+    klass.register(app, route_prefix='/v1.0/')
+
+  # TODO: Remove this HACK!
+  @app.errorhandler(404)
+  def page_not_found(e):
+    import urllib
+    output = ""
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        line = "<strong>%s</strong> %s %s" % (rule.endpoint, methods, urllib.unquote(url))
+        output += "<li>" + line + "</li>"
+
+    return """
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<title>404 Not Found</title>
+<h1>Not Found</h1>
+<p>The requested URL was not found on the server.  If you entered the URL manually please check your spelling and try again.</p>
+
+<h3>Current routes:</h3>
+<ul>
+%s
+</ul>
+    """ % output, 404
 
 def load_environment_config(app, env):
   if env not in STAGES:

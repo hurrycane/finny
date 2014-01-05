dc3d40ec9fa82f557cc83998e104da2e881af8de
Bogdan Gaza <bc.gaza@gmail.com>
2014-01-05 16:20:31 +0200

    Runner now supports endpoint as an generator argument

--- finny/runner.py
+++ finny/runner.py
@@ -39,7 +39,7 @@ class Finny(CLI):
   def run_command(self):
     command = CommandFactory.get(command=self.params.command,
                                  command_type=self.params.command_type)
-    command.run()
+    command.run(self.params)
 
   def generate_structure(self):
     path = self.params.path
@@ -93,10 +93,15 @@ def execute_from_cli():
     parser_runner = subparser_gen.add_parser('runner',
                                              help='Generate new runner')
 
+    parser_endpoint = subparser_gen.add_parser('endpoint',
+                                               help='Generate new endpoint')
+
     parser_default_runner  = subparser_gen.add_parser('default-runner',
                                                     help='Generate new runner')
 
     parser_runner.add_argument("name", help="Name of the runner")
+    parser_endpoint.add_argument("name", help="Name of the endpoint")
+
   else:
     subparsers = f.add_subparsers(help='sub-command help')
     parser_new = subparsers.add_parser('new', help='new help')

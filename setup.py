a07ec920f8faabb0e9d6b403450ccb4d84b335f5
Bogdan Gaza <bc.gaza@gmail.com>
2014-07-25 14:50:53 +0300

    Version bump to 0.3.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 requires = [ "pyyaml>=3.10" ]
 
 setup(name="finny",
-      version="0.3.4",
+      version="0.3.6",
       platforms='any',
       packages = find_packages(),
       include_package_data=True,
